da94181a26a34858ed62c0968d1c543fe06a3c35
NeoTryn <114293865+NeoTryn@users.noreply.github.com>
2024-12-06 14:12:44 +0100

    aufgabe 4 begonnen

--- kudos/app/routes/home.tsx
+++ kudos/app/routes/home.tsx
@@ -1,6 +1,6 @@
 // app/routes/home.tsx
-import { LoaderFunction, json } from '@remix-run/node'
-import { requireUserId } from '~/utils/auth.server'
+import { LoaderFunction} from '@remix-run/node'
+import { requireUserId, getUser } from '~/utils/auth.server'
 import { Layout } from '~/components/layout'
 import { UserPanel } from '~/components/user-panel'
 import { getOtherUsers } from '~/utils/user.server'
@@ -52,12 +52,13 @@ export const loader: LoaderFunction = async ({ request }) => {
 
   const kudos = await getFilteredKudos(userId, sortOptions, textFilter)
   const recentKudos = await getRecentKudos()
+  const user = await getUser(request)
 
-  return json({ users, kudos, recentKudos })
+  return Response.json({ users, kudos, recentKudos, user })
 }
 
 export default function Home() {
-  const { users, kudos, recentKudos } : any = useLoaderData()
+  const { users, kudos, recentKudos, user } : any = useLoaderData()
   return (
     <Layout>
       <Outlet />
@@ -65,7 +66,7 @@ export default function Home() {
         <UserPanel users={users} />
         <div className="flex-1 flex flex-col">
           {/* Search Bar Goes Here */}
-          <SearchBar />
+          <SearchBar profile={user.profile}/>
           <div className="flex-1 flex">
             <div className="w-full p-10 flex flex-col gap-y-4">
               {kudos.map((kudo: KudoWithProfile) => (
